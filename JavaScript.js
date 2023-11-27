10542a82ab638bcec695af54095d7752324325ce
henrJS <143142117+henrJS@users.noreply.github.com>
2023-11-27 20:08:34 -0300

    Create JavaScript.js
    JavaScript

---
+++ JavaScript.js
@@ -0,0 +1,39 @@
+function openNav(){
+    document.getElementById("myNav").style.width = '100%';
+}
+function closeNav(){
+    document.getElementById("myNav").style.width = '0%';
+}
+function gabNav(){
+    document.getElementById("gaboverlay").style.width='100%';
+}
+function placNav(){
+    document.getElementById("placoverlay").style.width= '100%';
+}
+function hdNav(){
+    document.getElementById("hdoverlay").style.width='100%';
+}
+function ramNav(){
+    document.getElementById("ramoverlay").style.width='100%';
+}
+function procsNav(){
+    document.getElementById("procsoverlay").style.width='100%';
+}
+function gabsNav(){
+    document.getElementById("gabsoverlay").style.width='100%';
+}
+function cuidNav(){
+    document.getElementById("cuidoverlay").style.width='100%';
+}
+function persNav(){
+    document.getElementById("persoverlay").style.width='100%';
+}
+function modelNav(){
+    document.getElementById("modeloverlay").style.width= '100%';
+}
+function imgNav(){
+    document.getElementById("imgoverlay").style.width= '100%';
+}
+function placsNav(){
+    document.getElementById("placsoverlay").style.width= '100%';
+}
